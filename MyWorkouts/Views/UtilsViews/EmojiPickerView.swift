bcbe07bb8aaa413004ee22da1b5437d8e67b6208
Владислав <103439608+lilvladick@users.noreply.github.com>
2025-07-31 17:17:58 +0300

    fix emoji for exercises

--- MyWorkouts/Views/UtilsViews/EmojiPickerView.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-struct EmojiPickerView: View {
-    var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
-    }
-}
